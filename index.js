4ba292ebb8550b144b7ece65dc7b88adfcc9c2f1
Sagar Saxena <sagardsaxena@gmail.com>
2020-04-04 22:44:52 +0000

    Host HooHacks Website

--- index.js
+++ index.js
@@ -27,9 +27,11 @@ app.use(cookieSession({
 // -------------- serve static folders -------------- //
 app.use('/home/js', express.static(path.join(__dirname, 'js')))
 app.use('/home/css', express.static(path.join(__dirname, 'css')))
+app.use('/documents', express.static(path.join(__dirname, 'Documents')))
 app.use('/landing/css', express.static(path.join(__dirname, 'Landing','css')))
 app.use('/landing/js', express.static(path.join(__dirname, 'Landing','js')))
-app.use('/documents', express.static(path.join(__dirname, 'Documents')))
+app.use('/covid/css', express.static(path.join(__dirname, 'HooHacks20','Website','css')))
+app.use('/covid/js', express.static(path.join(__dirname, 'HooHacks20','Website','js')))
 
 // -------------- express 'get' handlers -------------- //
 app.get('/', function(req, res){
@@ -49,6 +51,113 @@ app.get('/landing', function(req,res){
 app.get('/attendance', function(req,res){
 	console.log("Jumping To Attendance Card");
     res.redirect('/landing?jump=csl_card')
+});
+
+// ------------- HooHacks20 ------------- //
+const sgMail = require('@sendgrid/mail');
+const { MongoClient } = require('mongodb');
+
+sgMail.setApiKey('SG.YP5pBZoESoexi0rFM5KYEg.PP97oVi5RByg5qkSehkPHyEAZnzPszwhPhpewzjOBGw');
+const uri = "mongodb+srv://Hackers:HooHacks@hoohacks2020-czgp0.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+client.connect();
+
+// distinct past dates
+async function past(client){
+    var pastDates = await client.db("covid").collection("locations").distinct("Date");
+	console.log(pastDates)
+    return pastDates;
+}
+
+// distinct future dates
+async function future(client){
+    var futureDates = await client.db("covid").collection("predictions").distinct("Date");
+    return futureDates;
+}
+
+// data given a day that has passed
+async function dataOfPast(client, day){
+    var dataPast = await client.db("covid").collection("locations").find({Date: day}).toArray();
+    return dataPast;
+}
+
+// data given a day that has passed
+async function dataOfFuture(client, day){
+    var dataFuture = await client.db("covid").collection("predictions").find({Date: day}).toArray();
+    return dataFuture;
+}
+
+
+app.get('/covid', function(req,res){
+	console.log("Going To Website");
+    res.render(path.join(__dirname,'HooHacks20','Website','index.hbs')); 
+});
+
+app.get('/covid_dates', function(req, res) {
+	past(client).then(pst => {
+		future(client).then(fut => {
+		
+			var response = {
+				response: 200,
+				past: pst,
+				pred: fut
+			}
+
+			res.send(response)
+		});
+	});
+});
+
+app.get('/covid_data', function(req,res){
+	var date = req.query.date;
+	
+	dataOfPast(client, date).then(pst => {
+		dataOfFuture(client,date).then(fut => {
+			var response = {
+				response: 200,
+				past: pst,
+				pred: fut
+			}
+
+			res.send(response)
+
+		});
+	});
+});
+
+app.get('/covid_email', function(req,res){
+    var name = req.query.name;
+	var cases = req.query.cases;
+	var date = req.query.date;
+	var beds = req.query.beds;
+	var vents = req.query.vents;
+	var email = req.query.email;
+
+	var emSubject, emText;
+	emSubject = 'COVID-19 Report for location: '+ name;
+	emText = 'This is an update on COVID-19 at '+ name +
+		     '\n On Date' + date +
+         '\n'+'The number of Cases Reported are: ' + cases +
+         '\n'+'The Number of Ventilators needed are: '+ vents + 
+         '\n'+'The Number of Beds needed are: '+ beds; 
+	
+	console.log(emText)
+	console.log(req.query)
+
+	const msg = {
+  		to: email,
+ 		from: 'hitnuke@gmail.com',
+  		subject: emSubject,
+  		text: emText,
+  		html: emText,
+	};
+	sgMail.send(msg);
+
+    var response = {
+        response: 200,
+		data: req.query
+    }
+    res.send(response)
 });
 
 // -------------- listener -------------- //
